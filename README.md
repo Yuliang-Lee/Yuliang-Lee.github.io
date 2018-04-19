d7f20836a6e3fe29c770796e33d6dca0df5fbf62
Yuliang-Lee <frank_yll@163.com>
2018-04-19 16:26:29 +0800

    docs: update doc

--- README.md
+++ README.md
@@ -14,4 +14,4 @@ xlaoyu‘s blog repo
 
 使用了 [Service Worker](https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API) 把博客做成 PWA，提升访问速度，加强用户体验以及可以**`离线使用`**。如果使用手机浏览，在 Android 系统或者最新的 iOS 上可以把网页添加到桌面上，**当成 native app 使用**。
 
-Ps：如果使用本 blog 模板，本地运行时必须安装 Node，然后在项目根目录下运行 `npm install` 安装依赖，并且初始化 service worker 文件。
+Ps：如果使用本 blog 模板，本地运行时必须安装 Node，然后在项目根目录下运行 `npm install` 安装依赖。以后每次 `commit` 会自动扫描文件生成最新的 `service-worker` 文件。
--- _deprecated_sw.js
+++ _deprecated_sw.js
@@ -37,13 +37,13 @@ self.addEventListener('fetch', function (e) {
 
 // Cache resources
 self.addEventListener('install', function (e) {
-  e.waitUntil(Promise.all([
-      self.skipWaiting(),
-      caches.open(CACHE_NAME).then(function (cache) {
-        console.log('installing cache : ' + CACHE_NAME)
-        return cache.addAll(URLS)
-      })
-    ])
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(URLS)
+    }).then(_ => {
+      return self.skipWaiting()
+    })
   )
 })
 
